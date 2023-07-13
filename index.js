d6776d50fe4c9b1dbe00f94a2e03cd1f044a75d5
bivishan44 <bivishan44@gmail.com>
2023-07-13 15:17:41 +0545

    Second commit after validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// switching login and sign up
 $(window).on("hashchange", function () {
 	if (location.hash.slice(1) == "signup") {
 		$(".page").addClass("extend");
@@ -9,3 +10,56 @@ $(window).on("hashchange", function () {
 		$("#signup").removeClass("active");
 	}
 });
+
+//form validation
+$(window).trigger("hashchange");
+
+// login form validation
+function validateLoginForm(){
+	var email = document.getElementById("logEmail").value;
+	var password = document.getElementById("logPassword").value;
+
+	if(email == "" || password == ""){
+		document.getElementById("errorMsg").innerHTML = "Please fill the field first";
+		return false;
+	}
+
+	else if (password.length < 8){
+		document.getElementById("errorMsg").innerHTML = "Password must include 8 characters or more";
+		return false;
+	}
+
+	else{
+		alert("You are Successfully loged in Welcom to our Website");
+		return true;
+	}
+
+}
+
+// signup form validation
+function validateSignupForm(){
+	var name = document.getElementById("FullName").value;
+	var number = document.getElementById("MobileNumber").value;
+	var email = document.getElementById("SignEmail").value;
+	var password = document.getElementById("SignPassword").value;
+
+	if(name == "" || number == "" || email == "" || password == "" ){
+		document.getElementById("errorMsg").innerHTML = "Please all fill the required field";
+		return false;
+	}
+
+	else if ( number.length < 10 ){
+		document.getElementById("errorMsg").innerHTML = "Please include 10 numbers '(example=9812345687)'";
+		return false;
+	}
+	 
+	else if (password.length < 8){
+		document.getElementById("errorMsg").innerHTML = "Please enter atleast 8 characters";
+		return false;
+	}
+
+	else{
+		alert("You are Signed In Successfully");
+		return true;
+	}
+}
